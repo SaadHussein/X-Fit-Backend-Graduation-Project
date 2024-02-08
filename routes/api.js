ada3b16369b6dd744bb648b6ba914f11342ee2b3
SaadHussein <sa3dhusseein@gmail.com>
2024-02-08 15:15:01 +0200

    Delete Facebook Authentication because we do not have any bussiness organization

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const userRouter = require('./User/user.router');
 const GoogleUserRouter = require('./GoogleAuth/googleAuth.router');
-const FacebookRouter = require('./FacebookAuth/facebookAuth.router');
 const api = express.Router();
 
 api.use('/user', userRouter);
 api.use('/googleUser', GoogleUserRouter);
-api.use('/facebookUser', FacebookRouter);
 
 module.exports = api;
