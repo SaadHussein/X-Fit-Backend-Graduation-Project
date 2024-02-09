8a9b90ced70d94df9b82152e1c0a863775f0cb7b
SaadHussein <sa3dhusseein@gmail.com>
2024-02-09 15:28:00 +0200

    Trying to test google Authentication After Deploy

--- middleware/checkLoggedIn.js
+++ middleware/checkLoggedIn.js
@@ -1,7 +1,8 @@
 function checkedLoggedIn(req, res, next) {
     const isLoggedIn = req.isAuthenticated() && req.user;
     if (!isLoggedIn) {
-        return res.redirect('http://localhost:3000/failer');
+        // return res.redirect('http://localhost:3000/failer');
+        return res.redirect('https://x-fit-backend-graduation-project.vercel.app/failer');
     }
     next();
 }
