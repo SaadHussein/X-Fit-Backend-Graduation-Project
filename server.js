5f616d8d4bfd0417ea2d1b3ea4d6f31ebb524124
SaadHussein <sa3dhusseein@gmail.com>
2024-03-05 03:03:12 +0200

    handle unhandledRejection and uncaughtException errors

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 require('dotenv').config();
 require('express-async-errors');
+
+process.on('uncaughtException', (err) => {
+    console.log('Uncaught Exception, Shutting Down...');
+    console.log(err.name, err.message);
+    process.exit(1);
+});
+
 const http = require('http');
 const app = require('./app');
 const mongoDB = require('./helpers/mongo');
 
 const PORT = process.env.PORT || 3000;
+
 
 const server = http.createServer(app);
 
@@ -20,3 +28,11 @@ async function startServer() {
 }
 
 startServer();
+
+process.on('unhandledRejection', (err) => {
+    console.log('Unhandled Rejection, Shutting Down...');
+    console.log(err.name, err.message);
+    server.close(() => {
+        process.exit(1);
+    });
+});
